a795e2201fd92c26942a521a5258eeb2f01ffa18
hama2323 <hamamura0723@gmail.com>
2023-04-06 22:50:05 +0900

    Lesson17 Chapter9

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,8 +1,10 @@
 package com.techacademy.service;
 
 import java.util.List;
-import javax.transaction.Transactional;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -21,11 +23,26 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    // ----- 追加:ここから -----
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
-    // ----- 追加:ここまで -----
+
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id : idck) {
+            userRepository.deleteById(id);
+        }
+    }
+
+
+
 }
